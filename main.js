81c10b4510d1bbd59b8d96ae97614d0bae9f0f88
jakubbester <45968528+jakubbester@users.noreply.github.com>
2021-11-22 15:14:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,75 @@
-document.querySelector('#toggle-button').addEventListener('click', function() {
-  sideBarToggle++
-  if (sideBarToggle % 2 == 0) {
-    document.querySelector('#toggle-button').style.left = '320px'
-    document.querySelector('#side-bar').style.left = '0px'
-    document.querySelector('#main-page').style.marginLeft = '305px'
+function start() {
+  displayTime();
+  moveTime();
+  displayQRCode();
+}
+
+function displayTime() {
+  var today = new Date();
+  var year = today.getYear();
+  if (year < 1000) {
+    year += 1900;
+  }
+  var month = today.getMonth() + 1;
+  var day = today.getDate();
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var period;
+  if (hour < 12) {
+    period = "AM";
   }
   else {
-    document.querySelector('#toggle-button').style.left = '20px'
-    document.querySelector('#side-bar').style.left = '-300px'
-    document.querySelector('#main-page').style.marginLeft = '5px'
+    period = "PM";
   }
-})
+  if (hour == 24) {
+    hour = 0;
+  }
+  else if (hour > 12) {
+    hour = hour - 12;
+  }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+  document.getElementById("time_data").innerText = hour + ":" + minute + ":" + second + " " + period + " " + month + "/" + day + "/" + year;
+  setTimeout("displayTime()", 1000);
+}
+
+var count = 15;
+var movement = false;
+function moveTime() {
+  document.getElementById("time_data").style.left = count + "px";
+  if (movement == false) {
+    count++;
+    if (count == 135) {
+      movement = true;
+    }
+  }
+  else {
+    count--;
+    if (count == 15) {
+      movement = false;
+    }
+  }
+  setTimeout("moveTime()", 50);
+}
+
+function makeid(length) {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+var qrcode = new QRCode(document.getElementById("qrcode"), makeid(100));
+function displayQRCode() {
+  qrcode.clear();
+  qrcode.makeCode(makeid(100));
+  setTimeout("displayQRCode()", 3000);
+}
